2d9e36f0625e2e32beca26c5c5786af4fccc6bc9
Soskh2 <sofia.khemani@yale.edu>
2023-04-13 20:32:54 -0400

    coin images display on homepage

--- mysite/coins/views.py
+++ mysite/coins/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .utils import get_coin_images
 
 # Create your views here.
 
 # Home page/default
 def index(request):
-    return render(request, "coins/index.html", {})
+    images = get_coin_images()
+    return render(request, "coins/index.html", {"images": images})
 
 def coin_data(request):
     return render(request, "coins/coin_data.html", {})
